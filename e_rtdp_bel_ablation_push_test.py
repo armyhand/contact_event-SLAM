69199d9fab3e11fec02d7311c0e37f149e4524e4
armyhand <78639227+armyhand@users.noreply.github.com>
2026-07-24 17:08:39 +0800

    E-RTDP-Bel ablation test file

--- e_rtdp_bel_ablation_push_test.py
+++ e_rtdp_bel_ablation_push_test.py
@@ -14,7 +14,7 @@ from scipy.spatial.transform import Rotation as R, Slerp
 from scipy.spatial import KDTree
 
 """
-使用 E-RTDP-Bel 经验启发式的 blind push block 消融实验
+使用 E-RTDP-Bel 经验启发式的 blind push block 消融实验 
 """
 
 
